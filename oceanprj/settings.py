5211ae4c1ca72c2ed3a75243b7d387c3d54564c3
maymay120 <oceansfortunes@gmail.com>
2022-05-26 01:58:41 +0800

    the missels and the bombs sound like symphonies gone wrongh

--- oceanprj/settings.py
+++ oceanprj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(w3an%=5#2wbkhrolxuhpqu^ad6dh&4@ekcf381518qj-$fv-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 # CSRF_TRUSTED_ORIGINS = ['http://185.156.172.76','https://oceanfortunes.com','https://www.oceanfortunes.com']
